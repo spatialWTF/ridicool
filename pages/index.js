3f606cc092a1fd156eb5dc98a2f15159058f310b
spatialWTF <82198875+spatialWTF@users.noreply.github.com>
2022-02-11 21:04:11 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>ridicool nfts!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="coming up soon!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
